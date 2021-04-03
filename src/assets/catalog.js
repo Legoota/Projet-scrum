6dc3a0913b5bb8bc1ec4f59a0b3d2ee1789ad6a8
Léo K <leokrebs68@gmail.com>
2021-04-03 18:07:58 +0200

    Change image to using cache image

--- src/assets/catalog.js
+++ src/assets/catalog.js
@@ -3,56 +3,56 @@ export const catalog = [
     title: 'Les citrons',
     description: 'Des citrons. Idéal pour décorer une cuisine avec une touche de couleur.',
     prix: 20,
-    lien: 'http://localhost:8080/img/poster-citron.jpg',
+    lien: 'https://www.postery.com/media/catalog/product/cache/c67bd17d6d67468e1f797392976ffba9/i/l/il-limone-poster-2.jpg',
     rating: 5
   },
   {
     title: 'Le lac',
     description: 'Prenez un bol d\'air frais pendant votre confinement dans votre 10m2.',
     prix: 25,
-    lien: 'http://localhost:8080/img/poster-lac.jpg',
+    lien: 'https://www.postery.com/media/catalog/product/cache/c67bd17d6d67468e1f797392976ffba9/b/o/boat-in-lake-no2-poster-2.jpg',
     rating: 4
   },
   {
     title: 'Las Vegas',
     description: 'Profitez des casinos sans vous ruiner grâce à ce design.',
     prix: 15,
-    lien: 'http://localhost:8080/img/poster-lasvegas.jpg',
+    lien: 'https://www.postery.com/media/catalog/product/cache/c67bd17d6d67468e1f797392976ffba9/l/a/las-vegas-poster-2.jpg',
     rating: 5
   },
   {
     title: 'Papiers découpés',
     description: 'Faites vous passer pour quelqu\'un de cultivé grâce à ce design.',
     prix: 35,
-    lien: 'http://localhost:8080/img/poster-matisse.jpg',
+    lien: 'https://www.postery.com/media/catalog/product/cache/c67bd17d6d67468e1f797392976ffba9/m/a/matisse-cutout-blue-poster-2.jpg',
     rating: 4
   },
   {
     title: 'Noodles',
     description: 'Régalez vous sans grossir grâce à cette superbe oeuvre. #summerbody',
     prix: 30,
-    lien: 'http://localhost:8080/img/poster-noodles.jpg',
+    lien: 'https://www.postery.com/media/catalog/product/cache/c67bd17d6d67468e1f797392976ffba9/r/a/ramen-noodles-poster-1_1_1.jpg',
     rating: 3
   },
   {
     title: 'Système solaire',
-    description: 'Voyagez dans l\'espace et évadez vous quelques instants dans votre salon.',
+    description: 'Voyagez daxns l\'espace et évadez vous quelques instants dans votre salon.',
     prix: 40,
-    lien: 'http://localhost:8080/img/poster-planete.jpg',
+    lien: 'https://www.postery.com/media/catalog/product/cache/c67bd17d6d67468e1f797392976ffba9/s/o/solar-system-poster-2.jpg',
     rating: 4
   },
   {
     title: 'Paysage montagneux',
     description: 'Partez en vacances à la simple vue de ce design.',
     prix: 20,
-    lien: 'http://localhost:8080/img/poster-pont.jpg',
+    lien: 'https://www.postery.com/media/catalog/product/cache/c67bd17d6d67468e1f797392976ffba9/n/i/nine-arch-bridge-poster-2.jpg',
     rating: 5
   },
   {
     title: 'Vase',
     description: 'Une nature morte idéale pour égayer votre quotidien.',
     prix: 20,
-    lien: 'http://localhost:8080/img/poster-vase.jpg',
+    lien: 'https://www.postery.com/media/catalog/product/cache/c67bd17d6d67468e1f797392976ffba9/f/l/flower-vase-poster-2.jpg',
     rating: 4
   }
 ]
